d35c0c1a831fc5ad27ef4bdb321c51b245da74db
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-06-09 09:24:27 +0000

    h

--- waear/exts/configuration.py
+++ waear/exts/configuration.py
@@ -28,15 +28,19 @@ class Configuration(commands.Cog):
     async def admin_role(self: Self, interaction: Interaction, role: Role = SlashOption(name='admin_role', description='The admin role. People with admin perm always have this authority.')) -> None:
         """Set the admin role."""
         await interaction.client.db.set_admin_role(server_id=interaction.guild.id, role_id=role.id)
+        await interaction.send("I set the admin role to {}".format(role.mention), ephemeral=True)
     
     @authority.subcommand()
     async def mod_role(self: Self, interaction: Interaction, role: Role = SlashOption(name='mod_role', description='The mod role. People with admin perm always have this authority.')) -> None:
         """Set the mod role."""
         await interaction.client.db.set_mod_role(server_id=interaction.guild.id, role_id=role.id)
-    
+        await interaction.send("I set the mod role to {}".format(role.mention), ephemeral=True)
+
     @authority.subcommand()
     async def helper_role(self: Self, interaction: Interaction, role: Role = SlashOption(name='helper_role', description='The helper role. People with admin perm always have this authority.')) -> None:
         """Set the helper role."""
         await interaction.client.db.set_helper_role(server_id=interaction.guild.id, role_id=role.id)
+        await interaction.send("I set the helper role to {}".format(role.mention), ephemeral=True)
+
 def setup(bot: commands.Bot):
     bot.add_cog(Configuration(bot))
