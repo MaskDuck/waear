b5905491138db3f831353f4c56c41314854f6637
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-06-09 08:46:30 +0000

    h

--- waear/impl/database.py
+++ waear/impl/database.py
@@ -21,7 +21,7 @@ class Database:
     def set_initial_server(self, server_id: int):
         database = self.client.core.servers
         root_server = database.find_one({"type": "root"})
-        if root_server:
+        if root_server is not None:
             database.update_one({"type": "root"}, {"$set": {"_id": server_id}})
         else:
             database.insert_one({"_id": server_id, "type": "root"})
